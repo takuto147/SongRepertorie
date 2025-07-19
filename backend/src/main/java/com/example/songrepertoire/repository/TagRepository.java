e451208dc2f36cfe1b2f65848e42b4b93abd3d93
takuto147 <motherbasis39@gmail.com>
2025-07-20 08:30:49 +0900

    0720

--- backend/src/main/java/com/example/songrepertoire/repository/TagRepository.java
+++ backend/src/main/java/com/example/songrepertoire/repository/TagRepository.java
@@ -3,6 +3,5 @@ package com.example.songrepertoire.repository;
 import com.example.songrepertoire.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }

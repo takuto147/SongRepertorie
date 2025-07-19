9429241c16cd4d5573ebbccb14bd15b956db6ace
takuto147 <motherbasis39@gmail.com>
2025-07-19 22:36:46 +0900

    修正

--- backend/src/main/java/com/example/songrepertoire/repository/TagRepository.java
+++ backend/src/main/java/com/example/songrepertoire/repository/TagRepository.java
@@ -3,5 +3,6 @@ package com.example.songrepertoire.repository;
 import com.example.songrepertoire.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }
